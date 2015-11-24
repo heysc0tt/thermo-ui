c6c53dc364f92eab07f28e051eba67d5250fc0a0
Scott Johnson <scott.a.johnson@solutionset.com>
2015-11-24 09:28:42 -0800

    style changes etc

--- routes/index.js
+++ routes/index.js
@@ -12,50 +12,53 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/temps', function (req, res, next) {
-	var startTime;
-	if(req.query.st === undefined) {
-		var d = new Date();
-		d.setDate(d.getDate() - 1);
-		startTime = d;
-	} else {
-		console.log(req.query.st);
-		startTime = new Date(parseInt(req.query.st));
-	}
-	console.log(startTime);
+    var startTime;
+    if(req.query.st === undefined) {
+        var d = new Date();
+        d.setDate(d.getDate() - 1);
+        startTime = d;
+    } else {
+        startTime = new Date(parseInt(req.query.st));
+    }
+    
+    retrieveTempData(startTime, function(r) {
+        res.json(r);
+	});
+});
 
-	var MongoClient = require('mongodb').MongoClient
-
+function retrieveTempData(startTime, callback) { 
+    var MongoClient = require('mongodb').MongoClient;
     var connectionStr = 'mongodb://'+ mongoConfig.username +':'+mongoConfig.password+'@'+mongoConfig.host+':'+mongoConfig.port+'/'+mongoConfig.database;
-	MongoClient.connect(connectionStr, function(err, db) {
-	  if (err) {
-	    throw err;
-	  }
 
-	  db.collection("temperatures").find(
-	  	{ $and: [ {_id: { $gt: objectIdWithTimestamp(startTime) }}, {bbq: { $gt: 10, $lt: 25}}, {temperature: { $gt: 10, $lt: 25}}]}
+	MongoClient.connect(connectionStr, function(err, db) {
+      if (err) {
+        throw err;
+      }
+
+      db.collection("temperatures").find(
+        { $and: [ {_id: { $gt: objectIdWithTimestamp(startTime) }}, {bbq: { $gt: 0, $lt: 45}}, {temperature: { $gt: 10, $lt: 25}}]}
       ).sort(
         {_id: -1}
       ).map(function(t) { 
         return { bbq: (t.bbq*1.8) + 32, temp: (t.temperature*1.8) + 32, timestamp: t._id.getTimestamp().getTime() }
       })
       .toArray(function(err, result) {
-	    if (err) {
-	      throw err;
-	    }
-	    res.json(result);
-	  });
-	});
-
-});
+        if (err) {
+          throw err;
+        }
+		callback(result);
+      });
+    });
+}
 
 function objectIdMinusDays(days) {
-	var d = new Date();
-	d.setDate(d.getDate() - days);
-	return objectIdWithTimestamp(d);
+    var d = new Date();
+    d.setDate(d.getDate() - days);
+    return objectIdWithTimestamp(d);
 }
 
 function objectIdWithTimestamp(timestamp) {
-	var ObjectID = require('mongodb').ObjectID;
+    var ObjectID = require('mongodb').ObjectID;
 
     // Convert string date to Date object (otherwise assume timestamp is a date)
     if (typeof(timestamp) == 'string') {
@@ -72,13 +75,13 @@ function objectIdWithTimestamp(timestamp) {
 }
 
 function getMongoConfig() {
-	var mongoConfig = {};
-	mongoConfig.host = nconf.get('MONGO_HOST');
-	mongoConfig.port = nconf.get('MONGO_PORT');
-	mongoConfig.database = nconf.get('MONGO_DATABASE');
-	mongoConfig.username = nconf.get('MONGO_USERNAME');
-	mongoConfig.password = nconf.get('MONGO_PASSWORD');
-	return mongoConfig;
+    var mongoConfig = {};
+    mongoConfig.host = nconf.get('MONGO_HOST');
+    mongoConfig.port = nconf.get('MONGO_PORT');
+    mongoConfig.database = nconf.get('MONGO_DATABASE');
+    mongoConfig.username = nconf.get('MONGO_USERNAME');
+    mongoConfig.password = nconf.get('MONGO_PASSWORD');
+    return mongoConfig;
 }
 
 module.exports = router;
