2b565dee59af1d9a6cb212bcc3a052b54ab9216c
Scott Johnson <scott.a.johnson@solutionset.com>
2015-12-18 13:11:47 -0800

    style changes, cleanup code

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,4 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Benvenue Temps' });
 });
 
-// router.get('/temps', function (req, res, next) {
-//     var startTime;
-//     if(req.query.st === undefined) {
-//         var d = new Date();
-//         d.setDate(d.getDate() - 1);
-//         startTime = d;
-//     } else {
-//         startTime = new Date(parseInt(req.query.st));
-//     }
-    
-//     retrieveTempData(startTime, function(r) {
-//         res.json(r);
-// 	});
-// });
-
-
 module.exports = router;
